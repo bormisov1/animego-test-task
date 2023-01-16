ad1aa26e1cfe56a3bc98ea90181489ce59be62b7
dEN5 <moyadres_1@mail.ru>
2023-01-16 22:23:07 +0300

    for dev,do not use prod

--- src/modules/task/task.resolver.ts
+++ src/modules/task/task.resolver.ts
@@ -1,10 +1,46 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {ExecutionContext,Inject } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Status, Task, CreateTaskInput, UpdateTaskInput } from './task.model';
+import Redis from 'ioredis';
+import { CONTEXT } from '@nestjs/graphql';
+/*  */
 
 @Resolver(() => Task)
 export class TaskResolver {
-  constructor(private readonly taskService: TaskService) {}
+  private redis = Redis.createClient();
+  constructor(private readonly taskService: TaskService,
+    @Inject(CONTEXT) private readonly ctx: ExecutionContext
+    ){}
+
+
+
+  getIp(){
+    const request = this.ctx.switchToHttp().getRequest();
+    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+  return ip
+  }
+
+  async requestAsCache() {
+    try {
+      const key = this.getIp()
+      const getResponse = await this.redis.get(key);
+      if(getResponse) {
+        return JSON.parse(getResponse);
+      } else {
+        const setnxResponse = await this.redis.setnx(key, 5);
+        if (setnxResponse) {
+          await this.redis.expire(key, 3000 / 1000);
+        } else {
+          await this.redis.decrby(key, 1);
+        }
+        return false;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
 
   @Query((returns) => Task)
   async task(@Args('id') id: number): Promise<Task> {
@@ -19,14 +55,20 @@ export class TaskResolver {
       nullable: true,
     })
     status: Status,
-  ): Promise<Task[]> {
-    if (status === null) {
-      console.log('tasks');
-      return await this.taskService.getAll();
-    }
-
-    return await this.taskService.getByStatus(status);
-  }
+  ): Promise<Task[]>{
+    const cacheData = await this.requestAsCache();
+    if (cacheData) {
+      return {...cacheData, cache:true};
+    } else {
+      const queryResult = await this.taskService.getAll()
+      const ip = this.getIp()
+      this.redis.set(ip, JSON.stringify(queryResult), 'EX', 3000);
+      return queryResult;
+    } }
+  
+    /*
+    const queryResult = await this.taskService.getAll()
+     const data = await this.taskService.getByStatus(status); */
 
   @Mutation((returns) => Task)
   async createTask(
@@ -36,7 +78,6 @@ export class TaskResolver {
     return this.taskService.create(createTaskInput);
   }
 
-  
   @Mutation((returns) => Task)
   async updateTask(
     @Args({ name: 'updateTaskInput', type: () => UpdateTaskInput })
